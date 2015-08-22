74a2f9aff40c11bddbed4d68e61015fe6ddfb873
Aalsaunet <andreas.aalsaunet@gmail.com>
2015-08-22 16:26:16 +0200

    La til mer fancy ting i Calculator.lisp

--- Experimentation/Calculator.lisp
+++ Experimentation/Calculator.lisp
@@ -1,3 +1,15 @@
-(print "Skriv inn to tall som skal adderes")
+;;;;Call this function like this: (addTwoNumbers 10 20)
+(defun sum1(x y)
+  "Sum any two numbers after printing a message."
+  (format t "Summing ~d and ~d.~%" x y)
+  (+ x y ))
 
-(defun addTwoNumber(x, y))
+(defun sum2(x y)
+  "Sum any two numbers after printing a message."
+  (format t "~d + ~d = ~d.~%" x y (+ x y)))
+
+(defun sum3(a b  &optional (c 0) (d 0))
+  (+ a b c d))
+
+
+ 
