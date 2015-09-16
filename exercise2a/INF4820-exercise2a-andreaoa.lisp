cc57e922f044bc185b7ec0b7df8bf55203de07cd
Aalsaunet <andreas.aalsaunet@gmail.com>
2015-09-17 01:00:53 +0200

    Futher work on exercise2a

---
+++ exercise2a/INF4820-exercise2a-andreaoa.lisp
@@ -0,0 +1,47 @@
+;;;; Assignment 2a in INF4820, Autumn 2015
+;;;; Andreas Oven Aalsaunet, andreaoa@ifi.uio.no
+
+;;; Task 1A.
+;; There are several ways of defining context of words in  addition to the bag-of-words approuch
+;; this assignment will use. One alternative to the bag-of-words definition is to use context
+;; windows. With the context windows method we define context as the N number of words before
+;; and after the current word we are focusing on. If we have the sentence
+;; "Here are five given words" and we have "five" as our focus words and N = 1,
+;; then the features for "five" is ["are", "given"].
+
+;; We could also have defined context as grammatical context which looks at the grammatical
+;; relations to other words.
+
+;;; Task 2A
+(defparameter *stop-list*
+  '("a" "about" "also" "an" "and" "any" "are" "as" "at" "be" "been"
+    "but" "by" "can" "could" "do" "for" "from" "had" "has" "have"
+    "he" "her" "him" "his" "how" "i" "if" "in" "is" "it" "its" "la"
+    "may" "most" "new" "no" "not" "of" "on" "or" "she" "some" "such"
+    "than" "that" "the" "their" "them" "there" "these" "they" "this"
+    "those" "to" "was" "we" "were" "what" "when" "where" "which"
+    "who" "will" "with" "would" "you"))
+
+(defstruct (vs)
+  (matrix (make-hash-table :test #'equal))
+  similarity-fn)
+
+(defparameter vs-instance (make-vs)) 
+
+(defun read-words-to-hash (words)
+  (let ((file-stream (open words)))
+    (loop
+       for line = (read-line file-stream nil)
+       while line
+       do (setf (gethash line (vs-matrix vs-instance)) (make-hash-table :test #'equal))))
+  )
+
+(defun normalize-token (word)
+
+  )
+
+(defun read-corpus-to-vs (words)
+  (read-words-to-hash words)
+  )
+
+(defparameter space (read-corpus-to-vs "words.txt"))
